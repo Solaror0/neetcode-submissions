346a124bc974a675f7129e9098206c9c3b4d95a8
Jun Nur Mustaqeem <133717438+Solaror0@users.noreply.github.com>
2026-08-02 15:57:50 -0400

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.cpp
@@ -0,0 +1,32 @@
+class Solution {
+   public:
+    bool isValid(string s) {
+        std::stack<char> bracket;
+
+        // '(', ')', '{', '}', '[' and ']'
+
+        bool validity = true;
+
+        std::unordered_map<char, char> pairs = {
+
+            {'(', ')'}, {'{', '}'}, {'[', ']'}
+
+        };
+
+        if(s.length()==1){return false;}
+        for (char m : s) {
+            if (m == '(' || m == '[' || m == '{') {
+                bracket.push(m);
+            } else {
+                if(bracket.empty()){return false;}
+            
+                if (pairs[bracket.top()] != m) {
+                    return false;
+                }
+                bracket.pop();
+            }
+        }
+
+        if(bracket.empty()){return true;} else{ return false;}
+    }
+};
